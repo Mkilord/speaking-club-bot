499f1dd06c16af03ba2a79605788531ba5ff11a1
Mkilord <mkilord6666@gmail.com>
2024-12-19 16:08:09 +0300

    add phone number verification

--- node/src/main/java/ru/mkilord/node/TextUtils.java
+++ node/src/main/java/ru/mkilord/node/TextUtils.java
@@ -3,11 +3,25 @@ package ru.mkilord.node;
 import org.apache.commons.lang3.math.NumberUtils;
 
 public class TextUtils {
-    public static boolean isInRange(String str, int min, int max) {
+    public static boolean isRange(String str, int min, int max) {
+        if (str.isBlank()) return false;
         if (!NumberUtils.isCreatable(str)) {
             return false;
         }
         var num = Integer.parseInt(str);
         return num >= min && num <= max;
     }
+
+    public static boolean isProneNumber(String str) {
+        if (str.isBlank()) return false;
+        if (!NumberUtils.isCreatable(str)) {
+            return false;
+        }
+        return isSize(str, 11);
+    }
+
+    public static boolean isSize(String str, int minSize) {
+        return str.length() >= minSize;
+    }
+
 }
