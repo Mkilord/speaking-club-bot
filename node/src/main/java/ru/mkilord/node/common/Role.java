ca8947142b0c0bc6cbee2d02cd48406e7b415061
Mkilord <mkilord6666@gmail.com>
2024-12-19 17:59:04 +0300

    Remove role interface and create roles group by constants

--- node/src/main/java/ru/mkilord/node/common/Role.java
+++ node/src/main/java/ru/mkilord/node/common/Role.java
@@ -1,5 +1,23 @@
 package ru.mkilord.node.common;
 
-public interface Role{
-    String get();
+import lombok.experimental.FieldDefaults;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@FieldDefaults(level = PRIVATE, makeFinal = true)
+public enum Role {
+    USER(),
+    MEMBER(),
+    ORGANIZER(),
+    MODERATOR(),
+    ADMIN();
+
+    public static final Role[] EMPLOYEES = new Role[]{
+            MEMBER, ORGANIZER, MODERATOR, ADMIN
+    };
+    public static final Role[] ALL = new Role[]{USER, MEMBER, ORGANIZER, MODERATOR, ADMIN};
+    public static final Role[] MEMBER_EMPLOYEES = new Role[]{
+            MEMBER, ORGANIZER, MODERATOR, ADMIN
+    };
+
 }
