5b6c92f291cf291d38a3db7f5643cbf8d131fb7d
Mkilord <mkilord6666@gmail.com>
2024-12-24 14:23:09 +0300

    Remove of method

--- node/src/main/java/ru/mkilord/node/command/menu/Item.java
+++ node/src/main/java/ru/mkilord/node/command/menu/Item.java
@@ -5,8 +5,6 @@ import lombok.experimental.FieldDefaults;
 import ru.mkilord.node.command.MessageContext;
 import ru.mkilord.node.command.Step;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.function.Function;
@@ -24,10 +22,6 @@ public class Item {
 
     public Optional<Function<MessageContext, Step>> getOnClick() {
         return Optional.ofNullable(onClick);
-    }
-
-    public static List<Item> of(Item... items) {
-        return new ArrayList<>(List.of(items));
     }
 
     public Item(String text, Function<MessageContext, Step> onClick) {
