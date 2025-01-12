4b1dda366396c69d8a8bfcf7ea7331018e253fde
Mkilord <mkilord6666@gmail.com>
2025-01-12 14:33:07 +0300

    Add status group

--- node/src/main/java/ru/mkilord/node/model/enums/MeetStatus.java
+++ node/src/main/java/ru/mkilord/node/model/enums/MeetStatus.java
@@ -1,8 +1,13 @@
 package ru.mkilord.node.model.enums;
 
+import java.util.Set;
+
 public enum MeetStatus {
     PUBLISHED,
     HIDDEN,
     CANCELLED,
-    COMPLETED
+    COMPLETED;
+
+    public static final Set<MeetStatus> OLD_MEETS = Set.of(CANCELLED, COMPLETED);
+    public static final Set<MeetStatus> ACTUAL_MEETS = Set.of(PUBLISHED, HIDDEN);
 }
