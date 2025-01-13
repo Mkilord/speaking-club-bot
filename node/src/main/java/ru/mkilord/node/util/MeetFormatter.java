0ed164e78f7776a3937187d2c03bfe0819acd002
Mkilord <mkilord6666@gmail.com>
2025-01-13 16:17:05 +0300

    remove characters limit and add new methods

--- node/src/main/java/ru/mkilord/node/util/MeetFormatter.java
+++ node/src/main/java/ru/mkilord/node/util/MeetFormatter.java
@@ -1,24 +1,44 @@
 package ru.mkilord.node.util;
 
+import lombok.experimental.FieldDefaults;
 import ru.mkilord.node.model.Meet;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@FieldDefaults(level = PRIVATE, makeFinal = true)
 public class MeetFormatter {
-    public static String formatForMessage(Meet meet) {
-        return "\uD83D\uDCC6 %s\n\uD83D\uDD52 %s %s".formatted(meet.getDate(), meet.getTime(), meet.getName());
+    private static final DateTimeFormatter FULL_DATE_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter SHORT_DATE_FORMATTER = DateTimeFormatter.ofPattern("dd.MM");
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("H:mm");
+
+
+    public static String formatMeetWithOutStatus(Meet meet) {
+        var currentDate = LocalDate.now();
+        var formattedDate = meet.getDate().getYear() == currentDate.getYear()
+                ? meet.getDate().format(SHORT_DATE_FORMATTER)
+                : meet.getDate().format(FULL_DATE_FORMATTER);
+        var formattedTime = meet.getTime().format(TIME_FORMATTER);
+
+        return "\uD83D\uDCC6 %s \uD83D\uDD52 %s\n%s".formatted(formattedDate, formattedTime, meet.getName());
     }
 
-    public static String formatForItem(Meet meet) {
-        var croppedName = switch (meet.getStatus()) {
+    public static String formatForItemWithStatus(Meet meet) {
+        var currentDate = LocalDate.now();
+        var formattedDate = meet.getDate().getYear() == currentDate.getYear()
+                ? meet.getDate().format(SHORT_DATE_FORMATTER)
+                : meet.getDate().format(FULL_DATE_FORMATTER);
+        var formattedTime = meet.getTime().format(TIME_FORMATTER);
+
+        var title = switch (meet.getStatus()) {
             case COMPLETED -> "✅ ";
             case PUBLISHED -> "📢 ";
             case HIDDEN -> "🙈 ";
             case CANCELLED -> "❌ ";
         } + meet.getName();
 
-        if (meet.getName().length() > 30) {
-            croppedName = meet.getName().substring(0, 30) + "...";
-        }
-
-        return "\uD83D\uDCC6 %s\n\uD83D\uDD52 %s %s".formatted(meet.getDate(), meet.getTime(), croppedName);
+        return "\uD83D\uDCC6 %s \uD83D\uDD52 %s\n%s".formatted(formattedDate, formattedTime, title);
     }
 }
