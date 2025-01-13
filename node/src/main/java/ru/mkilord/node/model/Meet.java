6d188088e7df1c43ce70c3a66fa87d7d9fb05f3a
Mkilord <mkilord6666@gmail.com>
2025-01-13 13:19:48 +0300

    Change equals and hash

--- node/src/main/java/ru/mkilord/node/model/Meet.java
+++ node/src/main/java/ru/mkilord/node/model/Meet.java
@@ -1,10 +1,8 @@
 package ru.mkilord.node.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 import ru.mkilord.node.model.enums.MeetStatus;
 
@@ -27,9 +25,12 @@ public class Meet {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
 
+    @NotBlank
     String name;
 
+    @NotBlank
     LocalDate date;
+    @NotBlank
     LocalTime time;
 
     @ManyToMany
@@ -38,32 +39,27 @@ public class Meet {
             joinColumns = @JoinColumn(name = "meet_id"),
             inverseJoinColumns = @JoinColumn(name = "user_id")
     )
+    @ToString.Exclude
     Set<User> registeredUsers = new HashSet<>();
 
     @ManyToOne
     @JoinColumn(name = "club_id")
+    @ToString.Exclude
     Club club;
 
     @Enumerated(EnumType.STRING)
     MeetStatus status;
 
-    public String getInfo() {
-        var croppedName = name;
-        if (name.length() > 30) {
-            croppedName = name.substring(0, 30) + "...";
-        }
-        return "%s\n%s %s".formatted(croppedName, date, time);
-    }
-
     @Override
     public boolean equals(Object o) {
+        if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Meet meet = (Meet) o;
-        return id == meet.id && Objects.equals(name, meet.name) && Objects.equals(date, meet.date) && Objects.equals(time, meet.time) && Objects.equals(club, meet.club) && status == meet.status;
+        return id == meet.id;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, date, time, club, status);
+        return Objects.hash(id);
     }
 }
