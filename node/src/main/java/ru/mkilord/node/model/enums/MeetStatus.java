ee0eef4cf2cb936070485bdce0dce25a7a9ecc97
Mkilord <mkilord6666@gmail.com>
2025-01-14 11:49:44 +0300

    Remove meet status group

--- node/src/main/java/ru/mkilord/node/model/enums/MeetStatus.java
+++ node/src/main/java/ru/mkilord/node/model/enums/MeetStatus.java
@@ -1,13 +1,8 @@
 package ru.mkilord.node.model.enums;
 
-import java.util.Set;
-
 public enum MeetStatus {
     PUBLISHED,
     HIDDEN,
     CANCELLED,
-    COMPLETED;
-
-    public static final Set<MeetStatus> OLD_MEETS = Set.of(CANCELLED, COMPLETED);
-    public static final Set<MeetStatus> ACTUAL_MEETS = Set.of(PUBLISHED, HIDDEN);
+    COMPLETED
 }
